af2f9a732c3ed830fd2458de60f5f9835d1d0e3c
Bastian Weschasit <bastian.weschasit@iridium-works.com>
2024-02-06 19:35:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function sendMail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  fetch("https://formspree.io/f/xdoqeavd", {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(() => {
+    window.location.href = "./index_mail.html";
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
+function openPopup() {
+  let popup = document.getElementById("popup");
+  popup.style.display = "block";
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+}
+
+function closePopup() {
+  let popup = document.getElementById("popup");
+  popup.style.display = "none";
+  document.body.style.backgroundColor = "";
+  document.getElementsByName("email")[0].value = "";
+  document.getElementsByName("message")[0].value = "";
+}
+
+function sendMail(event) {
+  event.preventDefault();
+  openPopup();
+}
+
+// JAVASCRIPT FUNKTION FÜR PORTIONEN FEHLT
